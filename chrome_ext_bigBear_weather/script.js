cea3f3fc6931b854bafa394a8dd7c224b60795f6
Devin Yerasi <devinyerasi@yahoo.com>
2023-03-21 19:04:54 -0700

    Update script.js

--- chrome_ext_bigBear_weather/script.js
+++ chrome_ext_bigBear_weather/script.js
@@ -1,11 +1,5 @@
 async function getWeatherReport() {
-  const longitude = "34.2439";
-  const latitude = "-116.9114";
-
-  const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": "7151defa0cmsh3e9a8ebec89a299p19542ajsn47a48351b578",
+  const longitude = ",
       "X-RapidAPI-Host": "forecast9.p.rapidapi.com",
     },
   };
